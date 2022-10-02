c96a070bbbc399b0f7c14dbe71fba31f766faa08
yenifer Ortiz Herrera <105930050+yortizher@users.noreply.github.com>
2022-10-02 10:57:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-        name:"tinykoala528",
-        password:"thecat",
+        name:"",
+        password:"",
         arrayData:[],
         arrayDatosUser:[],
         error1:false,
